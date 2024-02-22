045ee23cf0707f5d3228dc5def7c759370b29fd0
alex <gajim91@gmail.com>
2024-02-22 13:10:02 -0600

    Primer comit del proyecyo gif del curso de FH

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  private _tagHistory: string[] = [];
+
+  constructor() { }
+
+  get tagsHistory(){
+    return [...this._tagHistory];
+  }
+
+  searchTag(tag:string): void{
+    this._tagHistory.unshift(tag);
+  }
+
+}
